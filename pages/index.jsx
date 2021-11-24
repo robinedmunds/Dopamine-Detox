62fb1cd5b949000c969d2ce2984abe9a709da66b
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2021-11-24 18:06:32 +0000

    Remove redundant modal footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -98,11 +98,6 @@ const index = () => {
                   </Tab>
                 </Tabs>
               </Modal.Body>
-
-              <Modal.Footer>
-                <Button variant="secondary">Close</Button>
-                <Button variant="primary">Save changes</Button>
-              </Modal.Footer>
             </Modal.Dialog>
           </Container>
         </Container>
